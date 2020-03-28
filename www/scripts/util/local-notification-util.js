1696c09787817f41362b744182225a5d9de9665f
Jay Lee <jaylee.possible@gmail.com>
2020-03-29 03:27:06 +0900

    .

--- www/scripts/util/local-notification-util.js
+++
@@ -1,1 +0,0 @@
-class LocalNotificationUtil{addNotification(i,o,t,a,c,n,f){if(!("LocalNotification"in window))throw new Error("LocalNotificaion is not exists.");window.LocalNotification.addNotification(i,o,t,a,c,n,f)}clearNotification(i,o){if(!("LocalNotification"in window))throw new Error("LocalNotificaion is not exists.");window.LocalNotification.clearNotification(i,o)}}
